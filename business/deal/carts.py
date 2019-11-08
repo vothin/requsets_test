04f8501266c196da20238b20abfc8c2e16a54a32
e <zy757161350@qq.com>
2019-11-08 10:39:25 +0800

    carts模块

--- business/deal/carts.py
+++ business/deal/carts.py
@@ -3,28 +3,29 @@
 '''
     @author: Vothin
     @software: 自动化测试
-    @file: o2o_cart.py
-    @time: 2019/11/5 16:34
+    @file: carts.py
+    @time: 2019/11/8 10:10
     @desc:
 '''
 # ********************************************************
 
-import requests
-from common.recordlog import logs
+
 from common.requests_test import Requests_Test
 from common.change_param import Change_Param
+from common.recordlog import logs
 
-class O2O_Cart(Requests_Test):
 
-    # 向o2o购物车添加商品
-    def post_o2o_catrs(self, username=None, password=None, data=None, prod=False):
+class Carts(Requests_Test):
+
+    # 向购物车中添加一个产品
+    def post_catrs(self, username=None, password=None, data=None, prod=False):
         '''
             相关参数有：  sku_id      产品ID            True
                         num         此产品的购买数量    True
                         activity    默认参与的活动id    False
         '''
 
-        self.suffix = self.c.get_value('Deal', 'o2o_carts')
+        self.suffix = self.c.get_value('Deal', 'carts')
         self.url = self.url_joint(prod)
 
         # 调用Change_Param类
@@ -36,10 +37,10 @@ class O2O_Cart(Requests_Test):
 
 
 
-    # 清空o2o购物车
-    def del_o2o_catrs_del(self, username=None, password=None, prod=False):
+    # 清空购物车
+    def del_catrs_del(self, username=None, password=None, prod=False):
 
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_del')
+        self.suffix = self.c.get_value('Deal', 'carts_del')
         self.url = self.url_joint(prod)
 
         # 调用Change_Param类
@@ -51,10 +52,10 @@ class O2O_Cart(Requests_Test):
 
 
 
-    # 获取o2o购物车页面的购物情况
-    def get_o2o_carts_all(self, username=None, password=None, prod=False):
+    # 获取购物车页面购物车详情
+    def get_carts_all(self, username=None, password=None, prod=False):
 
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_all')
+        self.suffix = self.c.get_value('Deal', 'carts_all')
         self.url = self.url_joint(prod)
 
         # 调用Change_Param类
@@ -66,15 +67,15 @@ class O2O_Cart(Requests_Test):
 
 
 
-    # o2o立即购买
-    def post_o2o_carts_buy(self, username=None, password=None, data=None, prod=False):
+    # 立即购买
+    def post_carts_buy(self, username=None, password=None, data=None, prod=False):
         '''
                     相关参数有：  sku_id      产品ID            True
                                 num         此产品的购买数量    True
                                 activity    默认参与的活动id    False
                 '''
 
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_buy')
+        self.suffix = self.c.get_value('Deal', 'carts_buy')
         self.url = self.url_joint(prod)
 
         # 调用Change_Param类
@@ -86,9 +87,9 @@ class O2O_Cart(Requests_Test):
 
 
 
-    # 获取结算页面O2O购物车详情
-    def get_o2o_carts_checked(self, username=None, password=None, prod=False):
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_checked')
+    # 获取结算页面购物车详情
+    def get_carts_checked(self, username=None, password=None, prod=False):
+        self.suffix = self.c.get_value('Deal', 'carts_checked')
         self.url = self.url_joint(prod)
 
         # 调用Change_Param类
@@ -101,11 +102,11 @@ class O2O_Cart(Requests_Test):
 
 
     # 设置全部商为选中或不选中
-    def post_o2o_carts_checked(self, username=None, password=None, data=None, prod=False):
+    def post_carts_checked(self, username=None, password=None, data=None, prod=False):
         '''
             相关参数有：  checked     是否选中,可用值:0,1
         '''
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_checked')
+        self.suffix = self.c.get_value('Deal', 'carts_checked')
         self.url = self.url_joint(prod)
 
         # 调用Change_Param类
@@ -118,12 +119,12 @@ class O2O_Cart(Requests_Test):
 
 
     # 批量设置某商家的商品为选中或不选中
-    def post_o2o_carts_seller(self, seller_id, username=None, password=None, data=None, prod=False):
+    def post_carts_seller(self, seller_id, username=None, password=None, data=None, prod=False):
         '''
             相关参数有：  seller_id   卖家id
                         checked     是否选中,可用值:0,1
         '''
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_seller')
+        self.suffix = self.c.get_value('Deal', 'carts_seller')
         self.suffix = self.suffix.format(seller_id)
         self.url = self.url_joint(prod)
 
@@ -137,13 +138,13 @@ class O2O_Cart(Requests_Test):
 
 
     # 更新O2O购物车中的多个产品
-    def post_o2o_carts_sku(self, sku_id, username=None, password=None, data=None, prod=False):
+    def post_carts_sku(self, sku_id, username=None, password=None, data=None, prod=False):
         '''
             相关参数有：  sku_id      产品id
                         checked     是否选中,可用值:0,1
                         num         产品数量
         '''
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_sku')
+        self.suffix = self.c.get_value('Deal', 'carts_sku')
         self.suffix = self.suffix.format(sku_id)
         self.url = self.url_joint(prod)
 
@@ -157,12 +158,12 @@ class O2O_Cart(Requests_Test):
 
 
     # 删除O2O购物车中的一个或多个产品
-    def del_o2o_carts_sku(self, sku_id, username=None, password=None, data=None, prod=False):
+    def del_carts_sku(self, sku_id, username=None, password=None, data=None, prod=False):
         '''
             相关参数有：  sku_id      产品id
                         checked     是否选中,可用值:0,1
         '''
-        self.suffix = self.c.get_value('Deal', 'o2o_carts_sku_del')
+        self.suffix = self.c.get_value('Deal', 'carts_sku_del')
         self.suffix = self.suffix.format(sku_id)
         self.url = self.url_joint(prod)
 
@@ -175,23 +176,12 @@ class O2O_Cart(Requests_Test):
 
 
 
-if __name__ == '__main__':
-    data = {
-        'sku_id' : '501',
-        'num' : '1'
-    }
 
-    c = O2O_Cart()
-    # result = c.post_o2o_catrs('13412345678', '123456', data, prod=True)
-    # result = c.get_o2o_carts_all('13412345678', '123456')
-    result = c.get_o2o_carts_all('13412345678', '123456')
-    # result = c.del_o2o_catrs_del('13412345678', '123456')
-    # result = c.get_o2o_carts_checked('13412345678', '123456')
+if __name__ == '__main__':
+    c = Carts()
+    result = c.get_carts_all('13412345678', '123456')
     print(result)
     print('响应正文：', result.text)
     print('响应头：', result.headers)
     print('响应url：', result.url)
     print('响应对应请求方式：', result.request)
-
-
-
