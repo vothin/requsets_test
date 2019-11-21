29a284b52c3ba47ae38c0d9efa43a4b36f1768af
e <zy757161350@qq.com>
2019-11-21 16:38:14 +0800

    promotion_groupbuy模块完成

--- business/promotion/promotion_groupbuy.py
+++ business/promotion/promotion_groupbuy.py
@@ -30,3 +30,46 @@ class Promotion_Groupbuy(Requests_Test):
         logs.info('test url:%s' % self.url)
 
         return self.get_requests(self.url, gu[0], data)
+
+
+
+    # 查询团购分类的所有标签
+    def get_promotions_groupbuy_cats(self, username=None, password=None, data=None, prod=None):
+
+        # 调用Change_Param类
+        cu = Change_Param(username, password, prod)
+        gu = cu.get_params()
+
+        # 拼接url
+        self.suffix = self.c.get_value('Promotion', 'promotions_groupbuy_cats')
+        self.url = self.url_joint(prod) + gu[1]
+        logs.info('test url:%s' % self.url)
+
+        return self.get_requests(self.url, gu[0], data)
+
+
+
+    # 查询团购商品
+    def get_promotions_groupbuy_goods(self, username=None, password=None, data=None, prod=None):
+
+        # 调用Change_Param类
+        cu = Change_Param(username, password, prod)
+        gu = cu.get_params()
+
+        # 拼接url
+        self.suffix = self.c.get_value('Promotion', 'promotions_groupbuy_goods')
+        self.url = self.url_joint(prod) + gu[1]
+        logs.info('test url:%s' % self.url)
+
+        return self.get_requests(self.url, gu[0], data)
+
+
+
+if __name__ == '__main__':
+    p = Promotion_Groupbuy()
+
+    # result = p.get_promotions_groupbuy_active()
+    # result = p.get_promotions_groupbuy_cats()
+    result = p.get_promotions_groupbuy_goods()
+    print(result)
+    print(result.text)
