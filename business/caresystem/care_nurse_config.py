d99c598fe84237b6dae44d921793705880776132
e <zy757161350@qq.com>
2020-01-03 17:31:52 +0800

    care_nurse_config模块完成

---
+++ business/caresystem/care_nurse_config.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+'''
+    @author: Vothin
+    @software: 自动化测试
+    @file: care_nurse_config.py
+    @time: 2020/1/3 17:31
+    @desc:
+'''
+# ********************************************************
