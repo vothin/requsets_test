0bbbe8efdbf96635370b487ee73ef27f42c1ab0a
e <zy757161350@qq.com>
2020-01-03 16:56:02 +0800

    care_employee模块完成

---
+++ business/caresystem/care_employee.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+'''
+    @author: Vothin
+    @software: 自动化测试
+    @file: care_employee.py
+    @time: 2020/1/3 16:55
+    @desc:
+'''
+# ********************************************************
