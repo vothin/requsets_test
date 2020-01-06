a072d5f1921e12dd9f4c007253d6bc4f4806bc3d
e <zy757161350@qq.com>
2020-01-06 14:23:20 +0800

    care_patient_info模块完成

---
+++ business/caresystem/care_patient_info.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+'''
+    @author: Vothin
+    @software: 自动化测试
+    @file: care_patient_info.py
+    @time: 2020/1/6 14:22
+    @desc:
+'''
+# ********************************************************
+
+from common.requests_test import Requests_Test
+from common.change_param import Change_Param
+from common.recordlog import logs
+
+class Care_Patient_Info(Requests_Test):
+
+    pass
