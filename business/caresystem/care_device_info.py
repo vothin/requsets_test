b0bc72529018c71bd1a3ff4158479eede223394c
e <zy757161350@qq.com>
2020-01-06 17:53:15 +0800

    care_device_info模块完成

---
+++ business/caresystem/care_device_info.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+'''
+    @author: Vothin
+    @software: 自动化测试
+    @file: care_device_info.py
+    @time: 2020/1/6 17:52
+    @desc:
+'''
+# ********************************************************
+
+
+from common.recordlog import logs
+from common.change_param import Change_Param
+from common.requests_test import Requests_Test
+
+class Care_Device_Info(Requests_Test):
